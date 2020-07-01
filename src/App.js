e1e4e18a5ba026a60388acf8f9a9cd4f7c248c91
aidenwaring <aiden.waring@icloud.com>
2020-07-01 13:27:44 +1000

    Changed to BrowserRouter Links to prevent browser from refreshing on navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react'
-import {BrowserRouter, Route} from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import HomeView from './HomeView'
 import CategorySelectionView from './CategorySelectionView'
 import NewEntryView from './NewEntryView'
 
 const App = () => {
-  //Getter, setter  //Define state. Initial state of home.
-  // const [page, setPage] = useState("home")
-
   return (
     <div>
       <BrowserRouter>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/category">Category</Link>
+          <Link to="/entry">New Entry</Link>
+        </nav>
         <Route exact path="/" component={HomeView} />
         <Route exact path="/category" component={CategorySelectionView} />
         <Route exact path="/entry" component={NewEntryView} />
