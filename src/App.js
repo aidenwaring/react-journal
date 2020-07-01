254c5a72e29d39ddd4195e94579c6301977c8227
aidenwaring <aiden.waring@icloud.com>
2020-07-01 11:49:40 +1000

    Added button and onClick routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import HomeView from './HomeView'
 import CategorySelectionView from './CategorySelectionView'
 import NewEntryView from './NewEntryView'
-import HomeView from './HomeView'
 
 const App = () => {
+  //Getter, setter  //Define state. Initial state of home.
+  const [page, setPage] = useState("home")
+
   return (
-    <div >
-          <HomeView />
-          <CategorySelectionView />
-          <NewEntryView />
+    
+    <div>
+      <nav>
+        <button onClick={() => setPage("home")}>Home</button>
+        <button onClick={() => setPage("category")}>Category</button>
+        <button onClick={() => setPage("newEntry")}>New Entry</button>
+      </nav>
+      {page == "home" && <HomeView />}
+      {page == "category" && <CategorySelectionView />}
+      {page == "newEntry" && <NewEntryView />}
     </div>
   )
 }
